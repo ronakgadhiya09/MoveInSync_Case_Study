a52be544c6aaa708a56c45421502e5fbb935b4f4
ronakgadhiya09 <120314724+ronakgadhiya09@users.noreply.github.com>
2024-08-06 22:37:31 +0530

    Add files via upload

---
+++ TimeRange.java
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime;
+
+public class TimeRange {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public TimeRange(LocalDateTime startTime, LocalDateTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public boolean overlaps(TimeRange other) {
+        return startTime.isBefore(other.endTime) && endTime.isAfter(other.startTime);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimeRange timeRange = (TimeRange) o;
+        return startTime.equals(timeRange.startTime) && endTime.equals(timeRange.endTime);
+    }
+}
